425f817a734e4124a6e224c7b45a9d1dee5d6680
TriniSuarezC <trinisuarezcarreras@gmail.com>
2025-09-24 16:40:48 -0300

    /balance/userid test

--- Spendee/tests/index.test.js
+++ Spendee/tests/index.test.js
@@ -55,3 +55,111 @@ describe("GET /gasto", () => {
     expect(res.body).toEqual(gastosMock);
   });
 });
+
+describe("GET /ingreso", () => {
+  let prisma;
+  beforeEach(() => {
+    prisma = new PrismaClient();
+  });
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+  it("Cuando no hay ingresos, la respuesta debe ser una lista vacía", async () => {
+    prisma.ingreso.findMany.mockResolvedValue([]);
+    const res = await request(app).get("/ingreso");
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toEqual([]);
+  });
+  it("Cuando hay ingresos, la respuesta debe ser una lista con los ingresos", async () => {
+    const ingresosMock = [
+      {
+        id: 1,
+        usuarioId: 1,
+        ingreso: 100,
+        montoAnterior: 0,
+        fecha: String(new Date()),
+      },
+      {
+        id: 2,
+        usuarioId: 1,
+        ingreso: 50,
+        montoAnterior: 100,
+        fecha: String(new Date()),
+      },
+    ];
+    prisma.ingreso.findMany.mockResolvedValue(ingresosMock);
+    const res = await request(app).get("/ingreso");
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toEqual(ingresosMock);
+  });
+});
+
+describe("GET /balance/userId", () => {
+  let prisma;
+  beforeEach(() => {
+    prisma = new PrismaClient();
+  });
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+  it("Cuando no hay ingresos ni gastos, el balance debe ser 0", async () => {
+    prisma.gasto.findMany.mockResolvedValue([]);
+    prisma.ingreso.findMany.mockResolvedValue([]);
+    const res = await request(app).get("/balance/0");
+    expect(res.statusCode).toBe(200);
+    expect(res.body.balance).toEqual(0);
+  });
+  it("Cuando gasto 100 e ingreso 200, el balance debe ser 100", async () => {
+    prisma.gasto.findMany.mockResolvedValue([
+      {
+        id: 1,
+        usuarioId: 0,
+        gasto: 100,
+        montoAnterior: 0,
+        fecha: String(new Date()),
+      },
+    ]);
+    prisma.ingreso.findMany.mockResolvedValue([
+      {
+        id: 1,
+        usuarioId: 0,
+        ingreso: 200,
+        montoAnterior: 0,
+        fecha: String(new Date()),
+      },
+    ]);
+    const res = await request(app).get("/balance/0");
+    expect(res.statusCode).toBe(200);
+    expect(res.body.balance).toEqual(100);
+  });
+  it("Cuando hay solo gastos por 100, el balance debe ser -100", async () => {
+    prisma.gasto.findMany.mockResolvedValue([
+      {
+        id: 1,
+        usuarioId: 0,
+        gasto: 100,
+        montoAnterior: 0,
+        fecha: String(new Date()),
+      },
+    ]);
+    prisma.ingreso.findMany.mockResolvedValue([]);
+    const res = await request(app).get("/balance/0");
+    expect(res.statusCode).toBe(200);
+    expect(res.body.balance).toEqual(-100);
+  });
+  it("Cuando hay solo ingresos por 100, el balance debe ser 100", async () => {
+    prisma.gasto.findMany.mockResolvedValue([]);
+    prisma.ingreso.findMany.mockResolvedValue([
+      {
+        id: 1,
+        usuarioId: 0,
+        ingreso: 100,
+        montoAnterior: 0,
+        fecha: String(new Date()),
+      },
+    ]);
+    const res = await request(app).get("/balance/0");
+    expect(res.statusCode).toBe(200);
+    expect(res.body.balance).toEqual(100);
+  });
+});
